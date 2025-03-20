43f41cf46156faa08e406b16c04c67da30b31734
alechoward-lab <alechoward41@gmail.com>
2025-03-20 10:35:18 -0600

    Update dashboard_hero_tier_list.py

--- dashboard_hero_tier_list.py
+++ dashboard_hero_tier_list.py
@@ -65,7 +65,8 @@ with col1:
     # Define preset weighting functions
     preset_options = {
         "General Power ~2 Player":              np.array([ 4, 2, 2, 2, 1, 2, 3, 1, 2, 2, 2, 1, 0, 0, 1]),
-        "Multiplayer 3/4 Player":               np.array([ 4, 1, 2, 2, 1, 5, 2, 3, 1, 7, 2, 5, 0, 0, 6]),
+        "Multiplayer 3 Player":                 np.array([ 4, 1, 2, 2, 1, 5, 2, 3, 1, 7, 2, 5, 0, 0, 6]),
+        "Multiplayer 4 Player":                 np.array([ 4, 1, 2, 2, 1, 5, 2, 3, 1, 7, 2, 5, 0, 0, 10]),
         "Solo (No Rush)":                       np.array([ 8, 3, 2, 4, 2, 2, 4, 1, 2, 2, 2, 1, 0, 4,-7]),
         "Solo Rush":                            np.array([ 0, 5, 0, 2, 5, 0, 0, 0, 0, 0, 0,-3, 0, 0, 0]),
         "Solo Final Boss Steady/Stalwart":      np.array([10, 3, 3, 8, 6, 2, 2, 4, 1, 2, 2, 2, 1,-4,-7]),

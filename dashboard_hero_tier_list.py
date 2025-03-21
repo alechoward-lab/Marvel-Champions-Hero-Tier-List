1f33eda96ddf281a8d470be31dc1ab850d8766b9
alechoward-lab <alechoward41@gmail.com>
2025-03-21 17:31:31 -0600

    moved socials bnanner to seapare tfile

--- dashboard_hero_tier_list.py
+++ dashboard_hero_tier_list.py
@@ -15,67 +15,7 @@ import json
 from hero_image_urls import hero_image_urls
 from default_heroes import default_heroes
 from preset_options import preset_options
-
-st.markdown(
-    """
-    <style>
-        .social-bar {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: transparent; /* Transparent background */
-            padding: 5px;
-            border: 2px solid white; /* White border */
-            border-radius: 8px;
-            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
-        }
-        .social-bar .left-content {
-            display: flex;
-            align-items: center;
-            margin-right: auto; /* Aligns logo and text to the left */
-        }
-        .social-bar .left-content .logo {
-            height: 40px;
-            margin-right: 10px;
-        }
-        .social-bar .social-links {
-            display: flex;
-            justify-content: center;
-        }
-        .social-links a {
-            margin-right: 20px; /* Increased margin between logos */
-            transition: opacity 0.3s ease-in-out;
-        }
-        .social-links a:hover {
-            opacity: 0.7;
-        }
-        .social-text {
-            font-family: Arial, sans-serif;
-            font-size: 16px;
-            font-weight: bold;
-            margin-right: 15px;
-            color: white;
-            text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.5);
-        }
-    </style>
-    <div class="social-bar">
-        <div class="left-content">
-            <img class="logo" src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/Daring_Lime_Logo.png?raw=true" alt="Your Logo">
-            <span class="social-text">Click to check out my socials:</span>
-        </div>
-        <div class="social-links">
-            <a href="https://www.youtube.com/channel/UCpV2UWmBTAeIKUso1LkeU2A" target="_blank">
-                <img src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/youtube_logo.png?raw=true" alt="YouTube" style="height: 30px;">
-            </a>
-            <a href="https://discord.gg/ReF5jDSHqV" target="_blank">
-                <img src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/Discord-Logo.png?raw=true" alt="Discord" style="height: 30px;">
-            </a>
-        </div>
-    </div>
-    """,
-    unsafe_allow_html=True
-)
-
+from socials_banner import socials_banner
 
 # ----------------------------------------
 # Define preset weighting options so update_preset can use them

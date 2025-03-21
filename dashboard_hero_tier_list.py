56bca2254cdc988e17dd69a7a6719f77f5e83529
alechoward-lab <alechoward41@gmail.com>
2025-03-21 15:50:21 -0600

    Update dashboard_hero_tier_list.py

--- dashboard_hero_tier_list.py
+++ dashboard_hero_tier_list.py
@@ -19,24 +19,48 @@ from preset_options import preset_options
 st.markdown(
     """
     <style>
+        .social-bar {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #FFFFFF;
+            padding: 5px;
+            border-radius: 8px;
+            box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.2);
+        }
         .social-links a {
             transition: opacity 0.3s ease-in-out;
         }
         .social-links a:hover {
             opacity: 0.7;
         }
+        .social-text {
+            font-family: Arial, sans-serif;
+            font-size: 16px;
+            font-weight: bold;
+            margin-right: 15px;
+        }
+        .logo {
+            height: 40px;
+            margin-right: 10px;
+        }
     </style>
-    <div class="social-links" style="display: flex; justify-content: center; align-items: center; background-color: #FFFFFF; padding: 5px;">
-        <a href="https://www.youtube.com/channel/UCpV2UWmBTAeIKUso1LkeU2A" target="_blank" style="margin-right: 10px;">
-            <img src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/youtube_logo.png?raw=true" alt="YouTube" style="height: 30px;">
-        </a>
-        <a href="https://discord.gg/ReF5jDSHqV" target="_blank" style="margin-right: 10px;">
-            <img src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/Discord-Logo.png?raw=true" alt="Discord" style="height: 30px;">
-        </a>
+    <div class="social-bar">
+        <img class="logo" src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/Daring_Lime_Logo.png?raw=true" alt="Your Logo">
+        <span class="social-text">Click to check out my socials:</span>
+        <div class="social-links">
+            <a href="https://www.youtube.com/channel/UCpV2UWmBTAeIKUso1LkeU2A" target="_blank" style="margin-right: 10px;">
+                <img src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/youtube_logo.png?raw=true" alt="YouTube" style="height: 30px;">
+            </a>
+            <a href="https://discord.gg/ReF5jDSHqV" target="_blank" style="margin-right: 10px;">
+                <img src="https://github.com/alechoward-lab/Marvel-Champions-Hero-Tier-List/blob/main/images/logo/Discord-Logo.png?raw=true" alt="Discord" style="height: 30px;">
+            </a>
+        </div>
     </div>
     """,
     unsafe_allow_html=True
 )
+
 
 
 # ----------------------------------------

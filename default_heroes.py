f33f02c5346e07f73c528c3e224d9ce8af085db0
alechoward-lab <alechoward41@gmail.com>
2025-04-04 19:05:49 -0600

    Add files via upload

--- default_heroes.py
+++ default_heroes.py
@@ -59,7 +59,7 @@ default_heroes = {          #              e, t, cv,s, d, th,re,mi,c, s, 0, br,l
         "Magik":                np.array([ 4, 1, 4, 3, 2, 4, 3, 3, 2, 0, 1, 1,-5, 5, 0]),
         "Iceman":               np.array([ 3, 2, 3, 3, 2, 2, 3, 4, 3, 2, 0, 0, 0, 0, 1]),
         "Jubilee":              np.array([ 3,-1, 4, 0, 2, 4, 3, 3, 4, 1, 0, 1,-1, 5, 1]),
-        "Nightcrawler":         np.array([ 1, 2, 3, 4, 1, 3, 4, 4, 1, 3, 0, 0,-1, 1, 2]),
+        "Nightcrawler":         np.array([ 1, 2, 3, 4, 1, 3, 4, 4, 1, 2, 0, 0,-1, 1, 2]),
         "Magneto":              np.array([ 3, 3, 3, 4, 3, 4, 5, 4, 2, 0, 0, 1, 3, 0, 1]),
                                 #          e, t, cv,s, d, th,re,mi,c, s, br,lg,si,sc,mu        
         "Maria Hill":           np.array([ 2, 1, 5, 1, 2, 5, 5, 1, 1, 2, 2, 5,-3, 0, 2]),

3508f18c9c3049629d2c5d63302c74e6c423c3a4
alechoward-lab <alechoward41@gmail.com>
2025-04-04 18:58:35 -0600

    Add files via upload

--- default_heroes.py
+++ default_heroes.py
@@ -20,7 +20,7 @@ default_heroes = {          #              e, t, cv,s, d, th,re,mi,c, s, 0, br,l
         "Scarlet Witch":        np.array([ 2, 3, 5, 3, 3, 2, 3, 1, 3, 4, 1, 0,-3, 2, 0]),
                                 #          e, t, cv,s, d, th,re,mi,c, s, br,lg,si,sc,mu          
         "Star-Lord":            np.array([ 4, 5, 3, 1, 5, 3, 2, 3,-3, 0, 1, 0,-5, 0, 0]),
-        "Groot":                np.array([ 0,-3, 2, 4, 3, 2,-2, 2, 0, 3, 0, 1, 2, 0, 2]),
+        "Groot":                np.array([ 0,-3, 2, 4, 3, 2,-2, 2, 0, 2, 0, 1, 2, 0, 2]),
         "Rocket":               np.array([ 3,-1, 0, 0,-2, 2,-2, 4, 0, 0, 1, 1,-3, 0, 1]),
         "Gamora":               np.array([ 1, 4, 3, 1, 3, 4, 4, 3, 0, 0, 1, 0, 3, 0, 0]),
         "Drax":                 np.array([ 2,-3, 4, 2, 4,-1,-5, 3, 0, 1, 1, 0,-2, 0, 0]),
